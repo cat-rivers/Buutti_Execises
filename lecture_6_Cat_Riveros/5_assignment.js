65981bcefc3db0f673b744251660ea1cc4efbd42
tvaisanen <vaisanen.toni@gmail.com>
2022-11-01 18:04:33 +0200

    Cat version 1.1
    - logic is the same but refactored into a fn

--- lecture_6_Cat_Riveros/5_assignment.js
+++ lecture_6_Cat_Riveros/5_assignment.js
@@ -9,36 +9,20 @@ let second;
 let third;
 let result = "";
 
+const compileText = (t1,t2,t3) => {
+    return [t1, (t2.length > t3.length
+                 ? [t2, t3]
+                 : [t3, t2])
+           ].flat().join(" ");
+}
+
+// here you check if the n1 is the longest?
 if (n1.length > n2.length && n1.length > n3.length) {
-  first = n1;
-  if (n2.length > n3.length) {
-    second = n2;
-    third = n3;
-  } else {
-    second = n3;
-    third = n2;
-  }
-  result = first + " " + second + " " + third;
+    result = compileText(n1, n2, n3);
 } else if (n2.length > n1.length && n2.length > n3.length) {
-  first = n2;
-  if (n1.length > n3.length) {
-    second = n1;
-    third = n3;
-  } else {
-    second = n3;
-    third = n1;
-  }
-  result = first + " " + second + " " + third;
+    result = compileText(n2, n1, n3);
 } else if (n3.length > n1.length && n3.length > n2.length) {
-  first = n3;
-  if (n2.length > n1.length) {
-    second = n2;
-    third = n1;
-  } else {
-    second = n1;
-    third = n2;
-  }
-  result = first + " " + second + " " + third;
+    result = compileText(n3, n1, n2);
 }
 
 console.log(result);
