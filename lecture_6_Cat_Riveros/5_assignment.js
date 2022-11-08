ca8595ca3de4811b63066549d58f17ee195528e7
Cat Rivers <cat.rivers.blog@gmail.com>
2022-11-08 13:06:33 +0200

    assignment 5 redo

--- lecture_6_Cat_Riveros/5_assignment.js
+++ lecture_6_Cat_Riveros/5_assignment.js
@@ -1,27 +1,31 @@
 // String length comparison
 
-const n1 = "Ana";
-const n2 = "lisa";
-const n3 = "lina";
+const name1 = "ii";
+const name2 = "lisa";
+const name3 = "catalina";
 let result;
 
-//This functions compiles and returns the ordered list. 
-//Requires only to find the longest length
-const textCompiler = (t1,t2,t3) => {
-  return [t1, (t2.length > t3.length
-               ?[t2 , t3] 
-               :[t3,t2])  
-         ].flat().join(", ")
-}
 
-//Checking the longest length and then using
-//The function to avoid redundancies
-
+const nameSorting = (n1, n2, n3)=> {
 if(n1.length > n2.length && n1.length > n3.length){
-    result = textCompiler(n1,n2,n3)
+        if(n2.length > n3.length){ return [n1 , n2 , n3] 
+        }else{
+            return [n1, n3, n2] 
+        }
 }else if(n2.length > n1.length && n2.length > n3.length){
-    result = textCompiler(n2,n1,n3)
-}else {
-    result = textCompiler(n3,n1,n2)
+        if(n1.length > n3.length){return [n2, n1, n3] 
+        }else{
+            return [n2, n3, n1] 
+        }
+}else{
+    if(n2.length > n1.length){
+        return [n3, n2, n1] 
+    }else{ 
+        return [n3, n1, n2] }
 }
-console.log(result)
+}
+
+console.log(nameSorting(name1, name2, name3))
+
+
+
