bab0ddaccad7bf33316e4ff4fe75170215210770
Cat Rivers <cat.rivers.blog@gmail.com>
2022-11-08 08:33:19 +0200

    lecture 8 files

---
+++ lecture_8_Cata_Riveros/1_assignment.js
@@ -0,0 +1,31 @@
+// a)
+const lang = "eng"
+
+const hello = (language)=>{
+    if(language === "es"){
+        console.log("Hola mundo!")
+    }
+    if(language === "eng"){
+        console.log("Hello world!")
+    }
+    if(language === "fi"){
+        console.log("Hei Maailman!")
+    }
+}
+
+hello(lang)
+
+// b)
+const hello2 = (language2)=>{
+    if(language2 === "es"){
+        console.log("Hola mundo!")
+    }
+    if(language2 === "eng"){
+        console.log("Hello world!")
+    }
+    if(language2 === "fi"){
+        console.log("Hei Maailman!")
+    }
+}
+
+hello2("es")
