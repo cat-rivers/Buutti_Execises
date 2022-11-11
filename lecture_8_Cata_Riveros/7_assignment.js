3635423d8a99c6d43688febacf12a9e07523fa25
Cat Rivers <cat.rivers.blog@gmail.com>
2022-11-11 15:45:44 +0200

    changed 7

--- lecture_8_Cata_Riveros/7_assignment.js
+++ lecture_8_Cata_Riveros/7_assignment.js
@@ -1,29 +1,27 @@
-const { count } = require("yargs");
 
 
-function checkSentenceVowels(sentence, vowel) {
+function checkSentenceVowels(sentence) {
   let totalVowelCount = 0;
   // Check how many different vowels we have in the sentence
   const countOfVowels = (vowel) => {
     let count = 0;
-    for (let i = 0; i < sentence.length; i++) {
+      for (let i = 0; i < sentence.length; i++) {
       if (sentence.charAt(i).toLowerCase() === vowel) {
         count++;
         totalVowelCount++;
       }
-    }
+      }
     console.log(` count of ${vowel.toUpperCase()} is ${count}`);
   };
-  // countOfVowels("a");
-  // countOfVowels("e");
-  // countOfVowels("i");
-  // countOfVowels("o");
-  // countOfVowels("u");
-  // countOfVowels("y");
-  console.log(vowel, count)
+  countOfVowels("a");
+  countOfVowels("e");
+  countOfVowels("i");
+  countOfVowels("o");
+  countOfVowels("u");
+  countOfVowels("y");
   console.log("total vowel count: " + totalVowelCount);
 }
 
-checkSentenceVowels("A wizard's job is to vex chumps quickly in fog.", "a");
+checkSentenceVowels("A wizard's job is to vex chumps quickly in fog.");
 
-checkSentenceVowels("aaaa bbb ccc eeee","a")
+checkSentenceVowels("aaaa bbb ccc eeee")
