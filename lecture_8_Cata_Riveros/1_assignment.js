1bc0047f187f310d7042e3ce6a7f7d3ce132c304
Cat Rivers <cat.rivers.blog@gmail.com>
2022-11-11 10:49:16 +0200

    test

--- lecture_8_Cata_Riveros/1_assignment.js
+++ lecture_8_Cata_Riveros/1_assignment.js
@@ -1,20 +1,11 @@
 // a) __________________________
 
-const lang = "fi";
+const lang = "es";
 
 const hello = (language) => {
-  if (language === "es") 
-  {
-    console.log("Hola mundo!");
-  }
-  if (language === "eng") 
-  {
-    console.log("Hello world!");
-  }
-  if (language === "fi") 
-  {
-    console.log("Hei Maailman!");
-  }
+  if (language === "es") {console.log("Hola mundo!"); }
+  if (language === "eng") {console.log("Hello world!"); }
+  if (language === "fi") {console.log("Hei Maailman!"); }
 };
 
 hello(lang);
@@ -22,15 +13,9 @@ hello(lang);
 // b)______________________________________
 
 const hello2 = (language2) => {
-  if (language2 === "es") {
-    console.log("Hola mundo!");
-  }
-  if (language2 === "eng") {
-    console.log("Hello world!");
-  }
-  if (language2 === "fi") {
-    console.log("Hei Maailman!");
-  }
+  if (language2 === "es") { console.log("Hola mundo!");}
+  if (language2 === "eng") {  console.log("Hello world!");}
+  if (language2 === "fi") { console.log("Hei Maailman!");}
 };
 
 hello("eng");
@@ -38,17 +23,13 @@ hello("eng");
 // Extra - function using switch instead of if-else__________________
 function languageHello(language3) {
   switch (language3) {
-    case "es":
-      console.log("Hola mundo");
+    case "es":  console.log("Hola mundo");
       break;
-    case "eng":
-      console.log("Hello world");
+    case "eng": console.log("Hello world");
       break;
-    case "fi":
-      console.log("Hei maailman!");
+    case "fi":  console.log("Hei maailman!");
       break;
-    default:
-      console.log("language not available");
+    default:  console.log("language not available");
   }
 }
 
--- lecture_8_Cata_Riveros/5_assignment.js
+++ lecture_8_Cata_Riveros/5_assignment.js
@@ -1,23 +1,52 @@
-
-
 const triangles = [
   {name: "first triangle", width: 7.0, length: 3.5},
   {name: "second triangle", width: 4.3, length: 6.4},
   {name: "third triangle", width: 5.5, length: 5.0},
 ];
 
-// ******* Version a  ********************************
+// *****Version A **** trying out array.map
 
-function versionA () { 
+function versionA() {
   
-  const areaOfTriangles = (object) => {
-  let result;
-  for (let i = 0; i < object.length; i++) {
-    result = (object[i].width * object[i].length) / 2;
-    object[i].area = result;
-  }
-  return result;
-};
+  const areaOfTriangle = (triangle) =>
+  (triangle.width * triangle.length) / 2;
+    
+  console.log("first triangle area: " + areaOfTriangle(triangles[0]))
+  console.log("second triangle area: " + areaOfTriangle(triangles[1]))
+  console.log("third triangle area: " + areaOfTriangle(triangles[2]))
+
+  const areas = triangles.map(areaOfTriangle)
+  const max = (a,v,i) => a.value > v 
+            ? a 
+            : {value: v, index: i};
+            
+  const maxArea = areas.reduce(max, {value: 0, index: -1})
+
+  console.log( 
+    triangles[maxArea.index].name +
+    " has the biggest area!")
+}
+
+versionA() 
+
+
+
+// ******* Version B  ***** using for loop
+
+
+
+function versionB () { 
+  
+  const areaOfTriangles = (object) => 
+  {
+    let result;
+    for (let i = 0; i < object.length; i++) 
+    {
+      result = (object[i].width * object[i].length) / 2;
+      object[i].area = result;
+    }
+      return result;
+  };
 
 areaOfTriangles(triangles);
 
@@ -25,32 +54,20 @@ console.log("Area of first triangle: " + triangles[0].area);
 console.log("Area of second triangle: " + triangles[1].area);
 console.log("Area of third triangle: " + triangles[2].area);
 
-}
-
-versionA()
-
-//************ Extra ************************
+// //************ Extra ************************
 
 const biggestTriangle = triangles.reduce((a, b) => {
-  return a.area > b.area ? a : b;
+          return a.area > b.area 
+          ? a 
+          : b;
 });
 
-console.log(biggestTriangle.name + " has the biggest area!");
-
-// **************** Version B ******************
-
-function versionB () {
-  
-  const areaOfTriangle = (triangle) => {
-    triangle.area = (triangle.width * triangle.length) / 2;
-    return triangle.area;
-  };
-  
-  console.log("first triangle: " + areaOfTriangle(triangles[0]))
-  console.log("second triangle: " + areaOfTriangle(triangles[1]))
-  console.log("third triangle: " + areaOfTriangle(triangles[2]))
-  
+console.log(
+  biggestTriangle.name +
+  " has the biggest area!");
 }
 
 versionB()
+
+
 
