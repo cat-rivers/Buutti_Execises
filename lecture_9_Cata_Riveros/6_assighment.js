79038c5983f3a27f58f6010bca46be96e22a0e04
Cat Rivers <cat.rivers.blog@gmail.com>
2022-11-22 09:55:54 +0200

    changed function argument names

--- lecture_9_Cata_Riveros/6_assighment.js
+++ lecture_9_Cata_Riveros/6_assighment.js
@@ -5,7 +5,7 @@ const numbers = [4, 19, 7, 1, 9, 22, 6, 13];
 function versionA() {
   const findLargest = (array) => {
     let temp = 0;
-    for (const num of numbers) {
+    for (const num of array) {
       temp = num > temp ? num : temp;
     }
     return temp;
