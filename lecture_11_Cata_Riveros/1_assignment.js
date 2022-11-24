afc2b5cfd61052f5ee464b4f27fa8792d35137d1
Cat Rivers <cat.rivers.blog@gmail.com>
2022-11-24 09:40:18 +0200

    Add assignment 1

---
+++ lecture_11_Cata_Riveros/1_assignment.js
@@ -0,0 +1,37 @@
+const lookUpScores = {
+    S: 8,
+    A: 6,
+    B: 4,
+    C: 3,
+    D: 2,
+    F: 0
+}
+
+// a) -----------------------
+const command = "AABBSDE"
+const arr1 = command.split("")
+
+const calculateTotalScore = (input) => {
+  let sum = 0;
+  for (l of input) {
+    sum += lookUpScores[l];
+  }
+  return sum;
+};
+
+console.log(calculateTotalScore("AABBCC"))
+console.log(calculateTotalScore("SSFFF"))
+console.log(calculateTotalScore("DFCBDABSB"))
+
+
+// b) -----------------------
+
+const calculateAverageScore = (input) => {
+    
+   return  calculateTotalScore(input)/input.length
+      
+}
+
+
+console.log(calculateAverageScore("DFCBDABSB"))
+console.log(calculateAverageScore("AABBCC"))
