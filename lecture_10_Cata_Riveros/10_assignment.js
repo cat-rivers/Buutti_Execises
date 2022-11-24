c47477df194bc7198a092ce7691ab50e1c845d43
Cat Rivers <cat.rivers.blog@gmail.com>
2022-11-24 15:01:55 +0200

    boo

--- lecture_10_Cata_Riveros/10_assignment.js
+++ lecture_10_Cata_Riveros/10_assignment.js
@@ -3,14 +3,45 @@
 const unSorted = [4, 19, 7, 1, 9, 22, 6, 13]
 
 function mergeSort (array){
+     
     if(array.length === 1){
         return array
     }else{
-        const midPoint = array.length / 2 
-        const right = array.slice(midPoint, array.length)
-        const left = array.slice(0 , midPoint)
-        return ( right , left)
+        const middle = Math.floor(array.length / 2)
+        const right = array.slice(middle , array.length)
+        const left =array.slice(0, middle)
+        
+        return  mergeSubLists(mergeSort(left) , mergeSort((right)) )
+
     }
+} 
+function mergeSubLists(left, right){
+    const resArr = [];
+    let leftIndex =0;
+    let rightIndex = 0;
+
+    while(leftIndex < left.length && rightIndex < right.length){
+        if(left[leftIndex] < right[rightIndex]){
+            resArr.push(left[leftIndex]);
+            leftIndex++;
+           
+        }else{
+            resArr.push(right[rightIndex]);
+            rightIndex++;
+            
+        }
+    }
+    // This last part I didnt do alone, got help > w <
+    if (left[leftIndex]) {	
+		const leftoverElements = left.slice(leftIndex)
+		resArr.push(...leftoverElements); 
+	} else {
+		const leftoverElements = right.slice(rightIndex)
+		resArr.push(...leftoverElements); 
+
+	}
+  return (resArr )
+
 } 
 
 console.log(mergeSort(unSorted))
--- lecture_10_Cata_Riveros/test.js
+++ lecture_10_Cata_Riveros/test.js
@@ -1,50 +1,5 @@
-
-const votersList = [
-    {name: "Bob", age: 30, voted: true},
-    {name: "Jake", age: 32, voted: true},
-    {name: "Kate", age: 25, voted: false},
-    {name: "Sam", age: 20, voted: false},
-    {name: "Phil", age: 21, voted: true},
-    {name: "Ed", age: 55, voted: true},
-    {name: "Tami", age: 54, voted: true},
-    {name: "Mary", age: 31, voted: false},
-    {name: "Becky", age: 43, voted: false},
-    {name: "Joey", age: 41, voted: true},
-    {name: "Jeff", age: 30, voted: true},
-    {name: "Zack", age: 19, voted: false},
-  ];
-
-const functions = [
-    (count, voter) => voter.age < 26 && voter.voted === true ? count + 1 : count,
-    (count, voter) => voter.age < 26 ? count + 1 : count,
-    (count, voter) => voter.age > 25 && voter.age < 36 && voter.voted === true? count + 1: count,
-    (count, voter) => voter.age > 25 && voter.age < 36 ? count + 1 : count,
-    (count, voter) => voter.age > 35 && voter.voted === true ? count + 1 : count,
-    (count, voter) => voter.age > 35 ? count + 1 : count  
-  ]
-    
-  
-function voterResults(array, func) {
-      
-    const results = functions.map(arg => array.reduce(arg , 0))
-
-    const voteResults = { 
-        numYoungVotes: results[0],
-        numYoungPeople: results[1],
-        numMidVotesPeople: results[2],
-        numMidsPeople: results[3],
-        numOldVotesPeople: results[4],
-        numOldsPeople: results[5] 
-    }  
-        
-    return voteResults 
-  }
-  
-  
-
-  console.log(voterResults(votersList))
-  
-
+console.log(
+  ` `)
   
 
 
