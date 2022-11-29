0ff61f6fdb71ce69b9ab797e4fdb2d1dabb6eff4
Cat Rivers <cat.rivers.blog@gmail.com>
2022-11-29 09:19:33 +0200

    replacing files

--- personal_Reference_Project/main.js
+++ personal_Reference_Project/main.js
@@ -1,1 +1,22 @@
 // Library node App
+import fs from "fs"
+
+const getDB = () => {
+    const data = fs.readFileSync("./db.json")
+    return JSON.parse(data.toString())
+}
+
+const updateDB = (updatedDB) => fs.writeFileSync("./db.json" , JSON.stringify(updatedDB))
+
+
+// const updateUser = () => {
+//     const db = getDB();
+    
+//     do something with the db
+    
+//     const updatedDB = functionThatChangesDB(...)
+    
+//     updateDB(updatedDB);
+// }
+
+
