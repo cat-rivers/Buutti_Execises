6356d36af5c776537b7ed556192f8c4c6a914104
Cat Rivers <cat.rivers.blog@gmail.com>
2022-11-30 18:01:17 +0200

    cleaned up some functions

--- personal_Reference_Project/tempt_test.js
+++ personal_Reference_Project/tempt_test.js
@@ -1,24 +1,6 @@
 const readlineSync = require("readline-sync");
 const tools = require("./tools.js");
 let loggedIn = false;
-const commandList = {
-  help: () => help(),
-  search: () => console.log("search by Id or name"),
-  exit: () => process.exit(),
-  login: "login()",
-  signup: "signup()",
-  borrow: "borrowBook()",
-  return: "returnBook()",
-  list: "printList()",
-  change_name: "changeName()",
-  remove_account: "deleteAccount()",
-  logout: () => console.log("goodbye"),
-};
-
-let greetings = readlineSync.question(
-  "Welcome to Oulun kirjasto\nGet the list of available commands by typing 'help'."
-);
-help();
 
 function exitLibrary() {
   process.stdout.write("Goodbye!");
@@ -30,23 +12,9 @@ function getCommand(input) {
   commandList[strInut]();
 }
 
-function help() {
-  while (greetings !== "help") {
-    greetings = readlineSync.question(
-      "Get the list of available commands by typing 'help'."
-    );
-  }
-
-  if (greetings === "help" && loggedIn === false) {
-    loggedOutList();
-    const index = readlineSync.question("What would you like to do?");
-    getCommand(index);
-  }
-
-  if (greetings === "help" && loggedIn) {
-    loggedInList();
-    const index = readlineSync.question("What would you like to do?");
-  }
+function help(state) {
+  state.loggedIn ? loggedInList() : loggedOutList();
+  return state;
 }
 
 function loggedOutList() {
@@ -62,17 +30,54 @@ function loggedInList() {
   }
 }
 
-const password = () => {
-  readlineSync.question("type password: ", {
-    hideEchoBack: true,
-  });
-};
-
-const logIn = () => {
+const logIn = (state) => {
   let idCorrect = false;
+
   const id = readlineSync.question("Type your ID number: ", {
     hideEchoBack: false,
   });
 
-  password();
+  const password = readlineSync.question("type password: ", {
+    hideEchoBack: true,
+  });
+
+  return {...state, loggedIn: true};
 };
+
+const commandList = {
+  help: help,
+  search: () => console.log("search by Id or name"),
+  exit: process.exit,
+  login: logIn,
+  signup: "signup()",
+  borrow: "borrowBook()",
+  return: "returnBook()",
+  list: "printList()",
+  change_name: "changeName()",
+  remove_account: "deleteAccount()",
+  logout: (state) => ({loggedIn: false}),
+};
+
+const greeting = `
+Welcome to Oulun kirjasto
+Get the list of available commands by typing 'help'.
+`;
+const prompt = "What would you like to do?";
+
+const startApp = () => {
+  // start the UI loop
+
+  let state = {loggedIn: false};
+
+  console.log(greeting);
+
+  while (true) {
+    console.log(state);
+
+    const commandName = readlineSync.question(prompt);
+
+    state = commandList[commandName] ? commandList[commandName](state) : state;
+  }
+};
+
+startApp();
