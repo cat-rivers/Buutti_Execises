6f0e7304a2145db3967ec6a75a754c5e2827ad72
Cat Rivers <cat.rivers.blog@gmail.com>
2022-12-20 21:12:01 +0200

    add asignment 3

--- lecutre_15_Cata_Riveros/3_assignment.js
+++ lecutre_15_Cata_Riveros/3_assignment.js
@@ -8,17 +8,7 @@ let posts = [
 	{
 		name: "cata",
 		id: 1,
-		post: "lorem ipsum 1",
-	},
-	{
-		name: "cata",
-		id: 2,
-		post: "lorem ipsum 2",
-	},
-	{
-		name: "cata",
-		id: 3,
-		post: "lorem ipsum 3",
+		post: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid temporibus quis qui, harum, tempore dolor laboriosam ducimus tenetur doloribus eius quibusdam quas, corporis libero sint sapiente earum ut excepturi numquam!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate odio id ratione, rerum a recusandae quisquam sed? Ratione sit architecto officiis molestiae quos tenetur? Nesciunt, laborum quos? Cupiditate, voluptatem. Non?",
 	},
 ];
 
@@ -37,17 +27,33 @@ function toggleFormVisibility() {
 	}
 }
 
-function newPost() {}
+function newPost(e) {
+	e.preventDefault();
+	const formName = document.getElementById("formName").value;
+	const formPost = document.getElementById("formPost").value;
+	const postId = new Date().getTime();
+	const postObj = {name: formName, id: postId, post: formPost};
+	posts.push(postObj);
+	toggleFormVisibility();
+	renderPost(postObj);
+	console.log(posts);
+	renderPosts();
+}
 
-function renderPost({post, name, id}) {
+function renderPost({name, id, post}) {
 	const newDiv = document.createElement("div");
+	["col-12", "my-3", "newDiv"].forEach((dv) => newDiv.classList.add(dv));
 	const newBtn = document.createElement("button");
+	["btn", "btn-danger", "px-5"].forEach((clss) => newBtn.classList.add(clss));
 	newBtn.innerHTML = "Delete";
 	const newP = document.createElement("p");
 	newP.innerHTML = post;
+	const newH2 = document.createElement("h2");
+	newH2.innerHTML = name;
 	newBtn.addEventListener("click", () => deletePost(id));
-	newDiv.appendChild(newP);
+	newDiv.appendChild(newH2);
 	newDiv.appendChild(newBtn);
+	newDiv.appendChild(newP);
 	return newDiv;
 }
 
@@ -75,3 +81,15 @@ function deletePost(postid) {
 }
 
 document.addEventListener("DOMContentLoaded", renderPosts);
+
+// function renderPost({post, name, id}) {
+// 	const newDiv = document.createElement("div");
+// 	const newBtn = document.createElement("button");
+// 	newBtn.innerHTML = "Delete";
+// 	const newP = document.createElement("p");
+// 	newP.innerHTML = post;
+// 	newBtn.addEventListener("click", () => deletePost(id));
+// 	newDiv.appendChild(newP);
+// 	newDiv.appendChild(newBtn);
+// 	return newDiv;
+// }
