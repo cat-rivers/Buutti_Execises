8e756b5324af38085edcb4eb80d1b14138ae66cd
Cat Rivers <cat.rivers.blog@gmail.com>
2023-01-03 10:33:41 +0200

    assignment 17

---
+++ lecture_17_cata_riveros/lecture-17-assignments/src/App.js
@@ -0,0 +1,67 @@
+function App() {
+	const fullname = "Cat Rivers ";
+	const age = 31;
+
+	const planetList = [
+		{name: "Hoth", climate: "Ice"},
+		{name: "Tattooine", climate: "Desert"},
+		{name: "Alderaan", climate: "Temperate"},
+		{name: "Mustafar", climate: "Volcanic"},
+	];
+
+	function Greetings(props) {
+		return (
+			<div>
+				<h1>Assignment 1 </h1>
+				<p>
+					My name is {props.name} and I am {props.age} years old!
+				</p>
+			</div>
+		);
+	}
+
+	function R2D2() {
+		return (
+			<div>
+				<h2> Assignment 2</h2>
+				<div className="r2d2">
+					<img src="./r2d2.jpg" alt="I am R2D2"></img>
+					<h2>Hello, I am R2D2!</h2>
+					<p>
+						<i>BeeYoop BeeDeepBoom Weeop DEEpaEEya!</i>
+					</p>
+				</div>
+			</div>
+		);
+	}
+
+	function Planets(props) {
+		return (
+			<div>
+				<h2> Assignment 3</h2>
+
+				<table className="tableOfPlanets">
+					<tr>
+						<th>Name</th>
+						<th>Climate</th>
+					</tr>
+					{props.array.map((planet) => (
+						<tr>
+							<td>{planet.name}</td> <td>{planet.climate}</td>
+						</tr>
+					))}
+				</table>
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			<Greetings name={fullname} age={age} />
+			<R2D2 image />
+			<Planets array={planetList} />
+		</div>
+	);
+}
+
+export default App;
