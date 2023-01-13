aee222807331743011dc94a242be222d112cb7e4
Cat Rivers <cat.rivers.blog@gmail.com>
2023-01-13 09:40:22 +0200

    deleted (delete,add,edit) files

--- lecture_19_Cata_Riveros/lecture_19/src/App.js
+++ lecture_19_Cata_Riveros/lecture_19/src/App.js
@@ -1,58 +1,50 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TodoNote from "./TodoNote"
-import "./App.css"
-import { useState } from 'react';
-import { text } from 'figlet';
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoNote from "./TodoNote";
+import "./App.css";
+import { useState } from "react";
+import { text } from "figlet";
 
-const App = () =>  {
-  
-  
+const App = () => {
   const defaultTodos = [
-    {id: 1, text: 'Buy potatoes', complete: false},
-    {id: 2, text: 'Make food', complete: true},
-    {id: 3, text: 'Exercise', complete: false},
-    {id: 4, text: 'Do the dishes', complete: false},
-    {id: 5, text: 'Floss the teeth', complete: false},
-    {id: 6, text: 'Play videogames', complete: true},
-]
+    { id: 1, text: "Buy potatoes", complete: false },
+    { id: 2, text: "Make food", complete: true },
+    { id: 3, text: "Exercise", complete: false },
+    { id: 4, text: "Do the dishes", complete: false },
+    { id: 5, text: "Floss the teeth", complete: false },
+    { id: 6, text: "Play videogames", complete: true },
+  ];
 
   const [todoList, setTodoList] = useState(defaultTodos);
 
-  const toggleCompletion = (id) => {
-		const updateTodoList = todoList.map((element) =>
-			id === element.id
-				? {...element, complete: !element.complete}
-				: element
-		);
+  const toggleCompletion = id => {
+    const updateTodoList = todoList.map(element =>
+      id === element.id ? { ...element, complete: !element.complete } : element
+    );
 
-		return setTodoList(updateTodoList);
+    return setTodoList(updateTodoList);
   };
 
   const editTodoText = (id, newText) => {
-    const updateTodoList = todoList.map((element) =>
-			id === element.id
-				? {...element, text: newText}
-				: element
-		);
+    const updateTodoList = todoList.map(element =>
+      id === element.id ? { ...element, text: newText } : element
+    );
     return setTodoList(updateTodoList);
-
   };
 
   return (
-		<div className="App">
-			{todoList.map((element) => (
-				<TodoNote
-					object={element}
-					id={element.id}
-					onCompletionToggle={() => {
-						toggleCompletion(element.id);
-					}}
-          onChange ={() => editTodoText(element.id , text)}
-				/>
-			))}
-		</div>
+    <div className="App">
+      {todoList.map(element => (
+        <TodoNote
+          object={element}
+          id={element.id}
+          onCompletionToggle={() => {
+            toggleCompletion(element.id);
+          }}
+          onChange={() => editTodoText(element.id, text)}
+        />
+      ))}
+    </div>
   );
-
-}
+};
 
 export default App;
--- lecture_19_Cata_Riveros/lecture_19/src/TodoNote.js
+++ lecture_19_Cata_Riveros/lecture_19/src/TodoNote.js
@@ -1,53 +1,51 @@
-import {useState} from "react";
-import {Button} from "react-bootstrap";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
-const TodoNote = ({object, id, onCompletionToggle, onChange}) => {
-	const [completed, setCompleted] = useState(object.complete);
-	const [editMode, setEditMode] = useState(false);
+const TodoNote = ({ object, id, onCompletionToggle, onChange }) => {
+  const [completed, setCompleted] = useState(object.complete);
+  const [editMode, setEditMode] = useState(false);
 
-	const editModeFalse = () => {
-		return (
-			<>
-				<p className="form-check-label">{object.text}</p>
-				<Button onClick={() => setEditMode(true)}>Edit </Button>
-			</>
-		);
-	};
+  const editModeFalse = () => {
+    return (
+      <>
+        <p className="form-check-label">{object.text}</p>
+        <Button onClick={() => setEditMode(true)}>Edit </Button>
+      </>
+    );
+  };
 
-	const editModeTrue = () => {
-		return (
-			<>
-				<input type="text" placeholder="new text" />
-				<button onClick={() => setEditMode(false)} name="sumbit">
-					Save
-				</button>
-			</>
-		);
-	};
+  const editModeTrue = () => {
+    return (
+      <>
+        <input type="text" placeholder="new text" />
+        <button onClick={() => setEditMode(false)} name="sumbit">
+          Save
+        </button>
+      </>
+    );
+  };
 
-	return (
-		<div
-			className={
-				completed ? "todo-note color-green" : " todo-note color-red"
-			}
-		>
-			<form>
-				<input
-					className="form-check-input"
-					type="checkbox"
-					checked={completed}
-					onClick={() => {
-						setCompleted(!completed);
-						onCompletionToggle(id);
-					}}
-				/>
-				{!editMode ? editModeFalse() : editModeTrue()}
+  return (
+    <div
+      className={completed ? "todo-note color-green" : " todo-note color-red"}
+    >
+      <form>
+        <input
+          className="form-check-input"
+          type="checkbox"
+          checked={completed}
+          onClick={() => {
+            setCompleted(!completed);
+            onCompletionToggle(id);
+          }}
+        />
+        {!editMode ? editModeFalse() : editModeTrue()}
 
-				<Button className="btn-close"></Button>
-			</form>
-		</div>
-	);
+        <Button className="btn-close"></Button>
+      </form>
+    </div>
+  );
 };
 
 export default TodoNote;
